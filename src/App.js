bb1da7b4e4f885e40e71551b0fce4433f8c81fc3
Jami Geetha Lakshmi Sowmya <22MH1A0521@acoe.edu.in>
2025-06-24 01:19:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/user/Home';
-import Dashboard from './pages/admin/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/user/Home";
+import Dashboard from "./pages/admin/Dashboard";
 
 function App() {
   return (
